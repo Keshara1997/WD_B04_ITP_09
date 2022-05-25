d7b641efdfc7a6a522c8c26288526689ca0160e2
navinthirarajahsansayan <88415880+navinthirarajahsansayan@users.noreply.github.com>
2022-05-25 23:31:14 +0530

    supplier management

---
+++ Supplier/backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection successfully");
+})
+
+const EmployeeRouter = require('./routes/Employee');
+const DeliveryRouter = require("./routes/Delivery");
+const SupplierRouter = require("./routes/Supplier");
+const StockRouter = require('./routes/Stock');
+const CustomerRouter = require('./routes/Customer');
+
+app.use('/Employee', EmployeeRouter);
+app.use("/Delivery", DeliveryRouter);
+app.use("/Supplier", SupplierRouter);
+app.use('/Stock', StockRouter);
+app.use('/Customer', CustomerRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
